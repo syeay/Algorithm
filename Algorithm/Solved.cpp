1bbb03096c903e1610ac8abbbb5012c55336a936
SY_mtvs <3hyoung2@gmail.com>
2024-01-15 16:08:59 +0900

    2667 24480

---
+++ Algorithm/Solved.cpp
@@ -0,0 +1,130 @@
+//24.01.15
+//백준 : 24480(깊이우선탐색-2)
+#if 0
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m, r;
+int cnt = 0;
+vector<vector<int>> v1;
+vector<int> v2;
+vector<bool> visited;
+
+void dfs(int num) {
+	if (visited[num]) return;
+	visited[num] = true;
+	cnt++;
+	v2[num - 1] = cnt;
+
+	for (int i = 0; i < v1[num].size(); i++) {
+		dfs(v1[num][i]);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n >> m >> r;
+	v1.assign(n + 1, vector<int>());
+	v2.assign(n + 1, 0);
+	visited.assign(n + 1, false);
+
+	for (int i = 0; i < m; i++) {
+		int u, v;
+		cin >> u >> v;
+		v1[u].push_back(v);
+		v1[v].push_back(u);
+	}
+
+	for (int i = 0; i <= n; i++) {
+		sort(v1[i].begin(), v1[i].end(), greater<>());
+	}
+
+	dfs(r);
+
+	for (int i = 0; i < n; i++) {
+		cout << v2[i] << "\n";
+	}
+
+	return 0;
+}
+#endif
+
+// 백준 : 2667(단지 번호 붙이기)
+#if 0
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, result = 0, result_cnt = 0;
+// 왼 위 오 아래
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+vector<vector<int>> v;
+vector<int> result_vec;
+vector<vector<int>> visited;
+
+void input() {
+	cin >> n;
+	v.assign(n, vector<int>(n));
+	visited.assign(n, vector<int>(n, 0));
+
+	for (int i = 0; i < n; i++) {
+		// 한 줄의 입력을 string으로 받기
+		string arr;
+		cin >> arr;
+		for (int j = 0; j < arr.length(); j++) {
+			// 문자는 '' 문자열일 때만 ""
+			if (arr[j] == '1')
+				v[i][j] = 1;
+			else
+				v[i][j] = 0;
+		}
+	}
+}
+
+void dfs(int x, int y) {
+	for (int i = 0; i < 4; i++) {
+		// 기준점을 중심으로 동서남북 체크
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+
+		// 움직인 값이 2차원 배열을 벗어날 경우
+		if (nx >= n || nx < 0 || ny >= n || ny < 0) continue;
+
+		// 아파트가 있고 방문하지 않았을 경우
+		if (v[nx][ny] == 1 && visited[nx][ny] == 0) {
+			visited[nx][ny] = 1;
+			result_cnt++;
+			dfs(nx, ny);
+		}
+	}
+}
+
+int main() {
+	input();
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (v[i][j] == 1 && visited[i][j] == 0) {
+				// 첫 시작점 방문 true
+				visited[i][j] = 1;
+				result_cnt = 1;
+				dfs(i, j);
+				result_vec.push_back(result_cnt);
+				result++;
+			}
+		}
+	}
+	// 오름차순 정렬
+	sort(result_vec.begin(), result_vec.end());
+	cout << result << "\n";
+	for (int i = 0; i < result_vec.size(); i++) {
+		cout << result_vec[i] << "\n";
+	}
+	return 0;
+}
+#endif
--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,60 +1,72 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-int n, m, r, cnt=0;
-vector<vector<int>> vec;
-queue<int> q1;
-vector<int> vec2;
-vector<bool> visited;
+int n, result=0, result_cnt=0;
+// 왼 위 오 아래
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+vector<vector<int>> v;
+vector<int> result_vec;
+vector<vector<int>> visited;
 
-void bfs(int num) {
-	q1.push(num);
-	visited[num] = true;
+void input() {
+	cin >> n;
+	v.assign(n, vector<int>(n));
+	visited.assign(n, vector<int>(n, 0));
 
-	while (!q1.empty()) {
-		int cur = q1.front();
-		q1.pop();
+	for (int i = 0; i < n; i++) {
+		// 한 줄의 입력을 string으로 받기
+		string arr;
+		cin >> arr;
+		for (int j = 0; j < arr.length(); j++) {
+			// 문자는 '' 문자열일 때만 ""
+			if(arr[j] == '1')
+				v[i][j] = 1;
+			else 
+				v[i][j] = 0;
+		}
+	}
+}
 
-		for (int i = 0; i < vec[cur].size(); i++) {
-			int next = vec[cur][i];
-			if (!visited[next]) {
-				cnt++;
-				vec2[next-1] = cnt;
-				q1.push(next);
-				visited[next] = true;
-			}
+void dfs(int x, int y) {
+	for (int i = 0; i < 4; i++) {
+		// 기준점을 중심으로 동서남북 체크
+		int nx = x+dx[i];
+		int ny = y+dy[i];
+
+		// 움직인 값이 2차원 배열을 벗어날 경우
+ 		if (nx >= n || nx < 0 || ny >= n || ny < 0) continue;
+		
+		// 아파트가 있고 방문하지 않았을 경우
+ 		if (v[nx][ny] == 1 && visited[nx][ny] == 0) {
+			visited[nx][ny] = 1;
+			result_cnt++;
+			dfs(nx, ny);
 		}
 	}
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(NULL);
-	cout.tie(0);
-	
-	cin >> n >> m >> r;
-	vec.assign(n+1, vector<int>());
-	visited.assign(n+1, false);
-	vec2.assign(n+1, 0);
-
-	for (int i = 0; i < m; i++) {
-		int u, v;
-		cin >> u >> v;
-		vec[u].push_back(v);
-		vec[v].push_back(u);
-	}
+	input();
 	for (int i = 0; i < n; i++) {
-		sort(vec[i].begin(), vec[i].end());
+		for (int j = 0; j < n; j++) {
+			if (v[i][j] == 1 && visited[i][j] == 0) {
+				// 첫 시작점 방문 true
+				visited[i][j] = 1;
+				result_cnt = 1;
+				dfs(i, j);
+				result_vec.push_back(result_cnt);
+				result++;
+			}
+		}
 	}
-
-	cnt = 1;
-	vec2[r-1] = cnt;
-	bfs(r);
-
-	for (int i = 0; i < n; i++) {
-		cout << vec2[i] << "\n";
+	// 오름차순 정렬
+	sort(result_vec.begin(), result_vec.end());
+	cout << result << "\n";
+	for (int i = 0; i < result_vec.size(); i++) {
+		cout << result_vec[i] <<"\n";
 	}
 	return 0;
 }
