272a1ead94230dd7ca384c8d0003333dbff75743
SY_mtvs <3hyoung2@gmail.com>
2024-01-24 17:03:57 +0900

    7562 bfs

--- Algorithm/Solved.cpp
+++ Algorithm/Solved.cpp
@@ -1,4 +1,71 @@
-//24,01.16
+//24.01.24
+//백준 : 7562(나이트의 이동)
+#if 0
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int n, l;
+int cur_x, cur_y;
+int tar_x, tar_y;
+int map[301][301];
+int visited[301][301];
+queue<pair<int, int>> q;
+
+int dx[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+int dy[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+
+void reset() {
+	while (!q.empty()) q.pop();
+	for (int i = 0; i < 301; i++) {
+		for (int j = 0; j < 301; j++) {
+			visited[i][j] = 0;
+			map[i][j] = 0;
+		}
+	}
+}
+
+void bfs(int x, int y) {
+	q.push({ x, y });
+	visited[x][y] = 1;
+	while (!q.empty()) {
+		int a = q.front().first;
+		int b = q.front().second;
+		q.pop();
+		if (a == tar_x && b == tar_y) {
+			cout << map[a][b] << "\n";
+			while (!q.empty()) {
+				q.pop();
+			}
+			break;
+		}
+		for (int i = 0; i < 8; i++) {
+			int new_a = a + dx[i];
+			int new_b = b + dy[i];
+			if (new_a < 0 || new_a >= l || new_b < 0 || new_b >= l) continue;
+			if (visited[new_a][new_b] == 0) {
+				q.push({ new_a, new_b });
+				visited[new_a][new_b] = 1;
+				map[new_a][new_b] = map[a][b] + 1;
+			}
+		}
+	}
+}
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> l;
+		cin >> cur_x >> cur_y;
+		cin >> tar_x >> tar_y;
+
+		bfs(cur_x, cur_y);
+		reset();
+	}
+}
+#endif
+
+//24.01.16
 //백준 : 2178(미로 탐색)
 #if 0
 #include <iostream>
--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,72 +1,62 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <queue>
 using namespace std;
 
-int n, result=0, result_cnt=0;
-// 왼 위 오 아래
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, 1, 0, -1};
-vector<vector<int>> v;
-vector<int> result_vec;
-vector<vector<int>> visited;
+int n, l;
+int cur_x, cur_y;
+int tar_x, tar_y;
+int map[301][301];
+int visited[301][301];
+queue<pair<int, int>> q;
 
-void input() {
-	cin >> n;
-	v.assign(n, vector<int>(n));
-	visited.assign(n, vector<int>(n, 0));
+int dx[] = {1, 2, 2, 1, -1, -2, -2, -1};
+int dy[] = {2, 1, -1, -2, -2, -1, 1, 2};
 
-	for (int i = 0; i < n; i++) {
-		// 한 줄의 입력을 string으로 받기
-		string arr;
-		cin >> arr;
-		for (int j = 0; j < arr.length(); j++) {
-			// 문자는 '' 문자열일 때만 ""
-			if(arr[j] == '1')
-				v[i][j] = 1;
-			else 
-				v[i][j] = 0;
+void reset() {
+	while(!q.empty()) q.pop();
+	for (int i = 0; i < 301; i++) {
+		for (int j = 0; j < 301; j++) {
+			visited[i][j]= 0;
+			map[i][j] = 0;
 		}
 	}
 }
 
-void dfs(int x, int y) {
-	for (int i = 0; i < 4; i++) {
-		// 기준점을 중심으로 동서남북 체크
-		int nx = x+dx[i];
-		int ny = y+dy[i];
-
-		// 움직인 값이 2차원 배열을 벗어날 경우
- 		if (nx >= n || nx < 0 || ny >= n || ny < 0) continue;
-		
-		// 아파트가 있고 방문하지 않았을 경우
- 		if (v[nx][ny] == 1 && visited[nx][ny] == 0) {
-			visited[nx][ny] = 1;
-			result_cnt++;
-			dfs(nx, ny);
+void bfs(int x, int y) {
+	q.push({x, y});
+	visited[x][y] = 1;
+	while (!q.empty()) {
+		int a = q.front().first;
+		int b = q.front().second;
+		q.pop();
+		if (a == tar_x && b == tar_y) {
+			cout << map[a][b] << "\n";
+			while (!q.empty()) {
+				q.pop();
+			}
+			break;
+		}
+		for (int i = 0; i < 8; i++) {
+			int new_a = a + dx[i];
+			int new_b = b + dy[i];
+			if (new_a < 0 || new_a >= l || new_b <0 || new_b >= l) continue;
+			if (visited[new_a][new_b] == 0) {
+				q.push({new_a, new_b});
+				visited[new_a][new_b] = 1;
+				map[new_a][new_b] = map[a][b]+1;
+			}
 		}
 	}
 }
 
 int main() {
-	input();
+	cin >> n;
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (v[i][j] == 1 && visited[i][j] == 0) {
-				// 첫 시작점 방문 true
-				visited[i][j] = 1;
-				result_cnt = 1;
-				dfs(i, j);
-				result_vec.push_back(result_cnt);
-				result++;
-			}
-		}
-	}
-	// 오름차순 정렬
-	sort(result_vec.begin(), result_vec.end());
-	cout << result << "\n";
-	for (int i = 0; i < result_vec.size(); i++) {
-		cout << result_vec[i] <<"\n";
-	}
-	return 0;
+		cin >> l;
+		cin >> cur_x >> cur_y;
+		cin >> tar_x >> tar_y;
+
+		bfs(cur_x, cur_y);
+		reset();
+	}	
 }
