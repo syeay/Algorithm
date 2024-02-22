ed528020d573d9a48e28cf5e26fdc8f53a6b6c25
SY_mtvs <112922846+syeay@users.noreply.github.com>
2024-02-22 15:04:51 +0900

    20002
    백준 20002 <사과나무>
    
    - 다중 for문 아닌 누적합 이용
    
    for문의 break, continue 기능
    - break -> for문 하나 벗어남
    - continue -> 해당 for문의 다음 차례로 넘어감

--- Algorithm/Solved.cpp
+++ Algorithm/Solved.cpp
@@ -1,3 +1,44 @@
+//24.02.22
+//백준 : 20002(사과나무)
+#if 0
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int n;
+int arr[301][301];
+int psum[301][301];
+
+void Solve() {
+	int answer = -1001;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			for (int k = 1; k <= n; k++) {
+				if (i - k < 0 || j - k < 0) break;
+				else {
+					answer = max(answer, psum[i][j] - psum[i - k][j] - psum[i][j - k] + psum[i - k][j - k]);
+				}
+			}
+		}
+	}
+	cout << answer;
+}
+
+int main() {
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> arr[i][j];
+			psum[i][j] = psum[i][j - 1] + psum[i - 1][j] + arr[i][j] - psum[i - 1][j - 1];
+		}
+	}
+
+	Solve();
+}
+#endif // 0
+
+
 //24.02.02
 //백준 : 11656번(접미사 배열)
 #if 0
--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,76 +1,35 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <map>
-#include <algorithm>
+#include <cmath>
+
 using namespace std;
 
-int r, c;
-int cnt=0;
-string table[1001][1001];
-map<string, int> m;
-map<string, int>::iterator it;
+int n;
+int arr[301][301];
+int psum[301][301];
 
-void recur() {
-	for (int i = r - 1; i >= 0; i--) {
-		for (int j = 0; j < c; j++) {
-			// 있을 경우
-			if (m.find(table[i][j]) != m.end()) {
-				if (table[i - 1][cnt] == table[i - 1][j]) cnt--;
-				else
-					it = m.find(table[i][j]);
-				cnt = it->second;
-				break;
-			}
-			// 없을 경우
-			else {
-				m.insert(make_pair(table[i][j], j));
+void Solve() {
+	int answer = -1001;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			for (int k = 1; k <= n; k++) {
+				if (i-k<0 || j-k<0) break;
+				else {
+					answer = max(answer, psum[i][j]-psum[i-k][j]-psum[i][j-k]+psum[i-k][j-k]);
+				}
 			}
 		}
 	}
+	cout << answer;
 }
 
 int main() {
-	
-	cin >> r >> c;
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < c; j++) {
-			char a;
-			cin >> a;
-			table[i][j] = a;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {			
+			cin >> arr[i][j];
+			psum[i][j] = psum[i][j-1]+psum[i-1][j]+arr[i][j]-psum[i-1][j-1];
 		}
 	}
 
-	//cout << table[0][1];
-
-
-	for (int i = r-1; i >= 0; i--) {
-		for (int j = 0; j < c; j++) {
-			// 있을 경우
-			if (m.find(table[i][j]) != m.end()) {
-				if (table[i-1][cnt] == table[i-1][j]) cnt--;
-				else 
-				it = m.find(table[i][j]);
-				cnt = it->second;
-				break;
-			}
-			// 없을 경우
-			else {
-				m.insert(make_pair(table[i][j], j));
-			}
-		}
-	}
-
-	table[r-1][j]
-
-	if (cnt == 0) cout << 0;
-	else cout << cnt-1;
-
-	// 가장 마지막 행(n) 검사
-	// 같은 문자가 있을 경우
-		// 해당 문자의 위의 행의 글자 비교
-		// 같을 경우 -> 3 반복
-		// 다를 경우 -> 해당 행(n) - 1
-	// 같은 문자가 없을 경우
-		// 0 반환
+	Solve();
 }
