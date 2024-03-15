284afa10081aa9b0f3c724649bf1541d2ddc7de7
SY_mtvs <3hyoung2@gmail.com>
2024-03-15 10:58:50 +0900

    9733
    백준 9733<꿀벌>
    
    - 소수점 아래 출력하는 방법
    - cout.fixed
    - cout.precision(n)

--- Algorithm/Solved.cpp
+++ Algorithm/Solved.cpp
@@ -1,3 +1,64 @@
+//24.03.15
+//¹éÁØ : 9733(²Ü¹ú)
+#if 0
+#include <iostream>
+#include <string>
+#include <vector>
+//#include <ctime>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	vector<int>  v;
+	v.assign(7, 0);
+	int cnt = 0;
+
+	for (int i = 0; i < 24; i++) {
+		string a;
+		cin >> a;
+		if (a != "")
+			cnt++;
+		if (a == "Re") {
+			v[0] += 1;
+		}
+		else if (a == "Pt") {
+			v[1] += 1;
+		}
+		else if (a == "Cc") {
+			v[2] += 1;
+		}
+		else if (a == "Ea") {
+			v[3] += 1;
+		}
+		else if (a == "Tb") {
+			v[4] += 1;
+		}
+		else if (a == "Cm") {
+			v[5] += 1;
+		}
+		else if (a == "Ex") {
+			v[6] += 1;
+		}
+		else
+			continue;
+	}
+
+	cout << fixed;
+	cout.precision(2);
+	cout << "Re " << v[0] << " " << v[0] / (double)cnt << "\n";
+	cout << "Pt " << v[1] << " " << v[1] / (double)cnt << "\n";
+	cout << "Cc " << v[2] << " " << v[2] / (double)cnt << "\n";
+	cout << "Ea " << v[3] << " " << v[3] / (double)cnt << "\n";
+	cout << "Tb " << v[4] << " " << v[4] / (double)cnt << "\n";
+	cout << "Cm " << v[5] << " " << v[5] / (double)cnt << "\n";
+	cout << "Ex " << v[6] << " " << v[6] / (double)cnt << "\n";
+	cout << "Total " << cnt << " 1.00";
+}
+#endif // 0
+
+
 //24.02.22
 //¹éÁØ : 20002(»ç°ú³ª¹«)
 #if 0
--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,32 +1,55 @@
 #include <iostream>
-#include <cmath>
-
+#include <string>
+#include <vector>
+//#include <ctime>
 using namespace std;
 
-int n, k;
-long long cnt = 0;
-int psum[200001];
-map
-
 int main() {
-	cin >> n >> k;
-	for (int i = 1; i <= n; i++) {
-		int a;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	vector<int>  v;
+	v.assign(7, 0);
+	int cnt = 0;
+
+	for (int i = 0; i < 24; i++) {
+		string a;
 		cin >> a;
-		psum[i] = psum[i-1] + a;
-	}
-
-	for (int i = 0; i < n; i++) {
-		int answer = 0;
-		for (int j = i; j < n; j++) {
-			// i~j까지의 누적합이 k일 경우
-			// i까지의 누적합 - k = j까지의 누적합
-			answer += 
-			if (psum[i]- k == psum[j]) cnt++;
+		if (a != "")
+			cnt++;
+		if (a == "Re") {
+			v[0] += 1;
 		}
+		else if (a == "Pt") {
+			v[1] += 1;
+		}
+		else if (a == "Cc") {
+			v[2] += 1;
+		}
+		else if (a == "Ea") {
+			v[3] += 1;
+		}
+		else if (a == "Tb") {
+			v[4] += 1;
+		}
+		else if (a == "Cm") {
+			v[5] += 1;
+		}
+		else if (a == "Ex") {
+			v[6] += 1;
+		}
+		else
+			continue;
 	}
 
-	
-
-	cout << cnt;
+	cout << fixed;
+	cout.precision(2);
+	cout << "Re " << v[0] << " " << v[0] / (double)cnt << "\n";
+	cout << "Pt " << v[1] << " " << v[1] / (double)cnt << "\n";
+	cout << "Cc " << v[2] << " " << v[2] / (double)cnt << "\n";
+	cout << "Ea " << v[3] << " " << v[3] / (double)cnt << "\n";
+	cout << "Tb " << v[4] << " " << v[4] / (double)cnt << "\n";
+	cout << "Cm " << v[5] << " " << v[5] / (double)cnt << "\n";
+	cout << "Ex " << v[6] << " " << v[6] / (double)cnt << "\n";
+	cout << "Total " << cnt << " 1.00";
 }
