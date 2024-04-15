abe295605d5d410707cf6151cb2b40838cca56cb
SY_mtvs <3hyoung2@gmail.com>
2024-04-15 22:20:19 +0900

    18311 푸는 중

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,43 +1,37 @@
 #include <vector>
-#include <algorithm>
 #include <iostream>
 using namespace std;
 
-int solution(vector<int>& A) {
-	int n = A.size();
-	if (n <= 2) return 0;
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-	vector<int> sumTile;
-	sumTile.assign(n, 0);
+	int n, k;
+	cin >> n >> k;
 
-	for (int i = 1; i < n; i++) {
-		sumTile[i] = A[i] + A[i-1];
-	}
-	
-	vector<int> map;
-	map.assign(n, 0);
-	map[1] = sumTile[1];
-
-	for (int i = 2; i < n; i++) {
-		map[i] = max(map[i-1], sumTile[i]);
-		if (i >2)
-			map[i] = max(map[i], map[i-3] + sumTile[i]);
+	vector<int> arr;
+	int sum = 0;
+	arr.push_back(0);
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		sum += a;
+		arr.push_back(sum);
 	}
 
-	int answer = map[n-1];
-	for (int i = n - 1; i > 2; i--) {
-		if (i >= 4)
-			answer = max(answer, map[i-4] + sumTile[i]);
-		answer = max(answer, map[i-2] + sumTile[i]);
-	}
-	
-	for (int i = 0; i < map.size(); i++) {
-		cout << map[i] << " ";
-	}
-	return answer;
-}
+	// 벡터에 값을 넣어놓는다.
+	//0 1 2 3 4 5
+	//7 4 2 4 5
+	//0 7 11 13 17 22
 
-int main() {
-	vector<int> a = { 1, 2, 3, 3, 2 };
-	solution(a);
+	if (k > sum) k = (sum*2-1) - k;
+
+	for (int i = 1; i < arr.size(); i++) {
+		if (k >= arr[i - 1] && k <= arr[i]) {
+			cout << i;
+			break;
+		}
+
+	}
 }
